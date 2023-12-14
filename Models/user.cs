3d1e3a667184a832d4a61368504b397fdac39bee
TienSy <hotiensy16062002@gmail.com>
2023-12-14 08:40:44 +0700

    Changge file user.cs by append declare interger i  = 1

--- Models/user.cs
+++ Models/user.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,7 @@ namespace BaiNgay3.Models
 {
     internal class user
     {
+        int i = 1;
+       
     }
 }
